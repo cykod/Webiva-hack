bd2622ae4f101addc5052391db65a571059b5994
Pascal Rettig <pascal@cykod.com>
2011-10-22 02:40:58 -0400

    adfsljkgaljkshfjklshfjlkas

--- app/controllers/hack/page_feature.rb
+++ app/controllers/hack/page_feature.rb
@@ -8,6 +8,7 @@ class Hack::PageFeature < ParagraphFeature
       <cms:next_link>Next!</cms:next_link>
       <span id='uplink' class='<cms:up>selected</cms:up>'><cms:rate_up_link>Up</cms:rate_up_link></span>
       <span id='downlink' class='<cms:down>selected</cms:down>'><cms:rate_down_link>Down</cms:rate_down_link></span>
+      <cms:url/>
       </cms:hack_idea>
   FEATURE
 
@@ -21,6 +22,39 @@ class Hack::PageFeature < ParagraphFeature
           :onclick => "Hack.next('#cmspara_#{paragraph.id}','#{ajax_url}')"
         }
       end
+
+    end
+  end
+
+  feature :hack_page_view, :default_feature => <<-FEATURE
+      <cms:hack_idea>
+      <h1><cms:title/></h1>
+      <cms:description/>
+      <cms:url/>
+      </cms:hack_idea>
+  FEATURE
+
+  def hack_page_view_feature(data)
+    webiva_feature(:hack_page_view,data) do |c|
+      c.expansion_tag('hack_idea') { |t| t.locals.hack_idea = data[:hack_idea] } 
+      hack_details(c,data)
+
+
+    end
+  end
+
+  def hack_details(c,data) 
+    c.h_tag('hack_idea:title') { |t|t.locals.hack_idea.title }
+    c.value_tag('hack_idea:description') { |t| simple_format(h(t.locals.hack_idea.description)) }
+    c.value_tag('hack_idea:desc_short') { |t| simple_format(h(truncate(t.locals.hack_idea.description, :length => t.attr['length'] || 200))) }
+    
+
+      c.image_tag('hack_idea:image') { |t| t.locals.hack_idea.image }
+      c.value_tag('hack_idea:votes') { |t| t.locals.hack_idea.votes }
+      c.value_tag('hack_idea:score') { |t| t.locals.hack_idea.score }
+
+      c.link_tag('hack_idea:') { |t| Configuration.link("/view/#{t.locals.hack_idea.permalink}") }
+
 
       c.link_tag('hack_idea:rate_up') do |t|
 
@@ -38,37 +72,12 @@ class Hack::PageFeature < ParagraphFeature
 
       end
 
+      c.expansion_tag('hack_idea:voted') { |t| data[:vote] && data[:vote].value.to_i != 0 }
+
+
       c.expansion_tag('hack_idea:up') { |t| data[:vote] && data[:vote].value.to_i > 0 }
       c.expansion_tag('hack_idea:down') { |t| data[:vote] && data[:vote].value.to_i < 0 }
-    end
-  end
 
-  feature :hack_page_view, :default_feature => <<-FEATURE
-      <cms:hack_idea>
-      <h1><cms:title/></h1>
-      <cms:description/>
-      </cms:hack_idea>
-  FEATURE
-
-  def hack_page_view_feature(data)
-    webiva_feature(:hack_page_view,data) do |c|
-      c.expansion_tag('hack_idea') { |t| t.locals.hack_idea = data[:hack_idea] } 
-      hack_details(c,data)
-
-
-    end
-  end
-
-  def hack_details(c,data) 
-    c.h_tag('hack_idea:title') { |t|t.locals.hack_idea.title }
-    c.value_tag('hack_idea:description') { |t| simple_format(h(t.locals.hack_idea.description)) }
-    
-
-      c.image_tag('hack_idea:image') { |t| t.locals.hack_idea.image }
-      c.value_tag('hack_idea:votes') { |t| t.locals.hack_idea.votes }
-      c.value_tag('hack_idea:score') { |t| t.locals.hack_idea.score }
-
-      c.link_tag('hack_idea:') { |t| "/view/#{t.locals.hack_idea.permalink}" }
   end
 
   feature :hack_page_submit, :default_feature => <<-FEATURE
